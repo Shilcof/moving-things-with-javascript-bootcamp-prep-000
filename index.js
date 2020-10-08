e027880db2d7695514b1cb2bf219cca6fdd8ded5
Robert Shilcof <robert.shilcof@gmail.com>
2020-10-08 15:21:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function moveDodgerLeft(dodger) {
     dodger.style.left = `${left - 1}px`
   }
 }
+
+function moveDodgerRight(dodger) {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left < 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
