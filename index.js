22966d033bbeebd37a9fc5cf22e228c6c7c1f98d
Robert Shilcof <robert.shilcof@gmail.com>
2020-10-08 15:23:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function moveDodgerLeft(dodger) {
 
 function moveDodgerRight(dodger) {
   var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(righttNumbers, 10)
+  var right = parseInt(rightNumbers, 10)
  
   if (right > 0) {
     dodger.style.right = `${right - 1}px`
