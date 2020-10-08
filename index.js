6829ab65441f07d354f8d0ed83f6950edfa46906
Robert Shilcof <robert.shilcof@gmail.com>
2020-10-08 15:22:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight(dodger) {
   var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(leftNumbers, 10)
  
-  if (left < 360) {
+  if (right > 0) {
     dodger.style.left = `${left + 1}px`
   }
 }
